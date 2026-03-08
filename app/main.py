432b11b0173fb23cbd3aa90f517e224004a9cc3e
tejendraraom <tejendraraom1998@gmail.com>
2026-03-08 11:06:52 +0530

    docker fix

--- app/main.py
+++ app/main.py
@@ -55,4 +55,4 @@ def health():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
